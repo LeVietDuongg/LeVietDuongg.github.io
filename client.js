8c477e97788f688400af74df7182f8a4319f0915
LeVietDuongg <22520300@gmail.com>
2025-01-15 23:59:19 +0700

    s

--- client.js
+++ client.js
@@ -4,6 +4,8 @@ const socket = io("https://backend-chat-production-9f87.up.railway.app");
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
+const usernameForm = document.getElementById('usernameForm');
+const usernameInput = document.getElementById('usernameInput');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,10 +15,24 @@ form.addEventListener('submit', (e) => {
     }
 });
 
+usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    socket.emit('setUsername', usernameInput.value);
+    usernameInput.value = '';
+});
+
 // Receive message from server and display
+socket.on('initMessages', (msgs) => {
+    msgs.forEach((msg) => {
+        const item = document.createElement('li');
+        item.textContent = `${msg.user} at ${msg.time}: ${msg.text}`;
+        messages.appendChild(item);
+    });
+});
+
 socket.on('chat message', (msg) => {
     const item = document.createElement('li');
-    item.textContent = msg;
+    item.textContent = `${msg.user} at ${msg.time}: ${msg.text}`;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
